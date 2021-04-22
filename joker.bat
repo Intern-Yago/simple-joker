e04e8b52b870cb31b344c5786102be6ce158e2d6
Syri-Yagowo <yagovictorbotafogo@gmail.com>
2021-04-21 21:25:20 -0300

    add shutdown

--- joker.bat
+++ joker.bat
@@ -5,3 +5,7 @@ echo oiiii
 echo espero que goste
 echo aproveite o presentinho
 timeout 5
+"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --profile"1" https://www.youtube.com/watch?v=cCkw5V9LCyY
+timeout 20
+shutdown -r -t 30 -f -c "Seu pc esta infectado agora, obrigado pelos dados"
+
